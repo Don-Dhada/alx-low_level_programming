ed1e53bbc0f2ff0a39ba1fe40f8dd9614c1178db
Adobunu Onu David <56542921+Don-Dhada@users.noreply.github.com>
2023-02-27 21:35:45 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * _strlen - Returns the length of a string.
  * @str: The string to get the length of.
  *
  * Return: The length of @str.
  */
-
 int _strlen(char *s)
 {
 	int longi = 0;
